133fb661e046f080db0aef3a3f3db6c78235f35c
abc <gagan.gagan211@gmail.com>
2020-05-09 20:03:38 +0530

    Styled Combined Project

--- app.js
+++ app.js
@@ -46,67 +46,60 @@ app.get("/", function(req, res) {
   axios.get("https://corona.lmao.ninja/v2/historical/all")
     .then(function(response) {
       timeline = response.data;
+      axios.get(URL2)
+        .then(function(response) {
+          global_top_card_data = response.data[0];
+          india_top_card_data_new=response.data[13];
+          for (var i = 0; i < 11; i++) {
+            perMillionCases.push(response.data[i]);
+            perMillionCasesCountry.push(response.data[i].country);
+          }
+
+        //  console.log(perMillionCases); //do not delete this
+          console.log(perMillionCasesCountry[3]);
+          axios.get(URL)
+            .then(function(response) {
+              var india_top_card_date = response.data.Countries;
+              india_top_card_date.forEach(function(item) {
+                if (item.Country === "India") {
+                  specific_india_top_card_data = item;
+                }
+              })
+              top_gcountry = india_top_card_date;
+              top_gcountry.sort(function(a, b) {
+                return b.TotalConfirmed - a.TotalConfirmed
+              });
+              for (var i = 0; i < 5; i++) {
+                piedata2.push(top_gcountry[i]);
+              }
+              for (var i = 0; i < 10; i++) {
+                usedtop_gcountry.push(top_gcountry[i]);
+                top_gconfirmed.push(top_gcountry[i].TotalConfirmed);
+                top_grecover.push(top_gcountry[i].TotalRecovered);
+                top_gdeaths.push(top_gcountry[i].TotalDeaths);
+              }
+              res.render("index", {
+                global_top_card_data: global_top_card_data,
+                forindia: specific_india_top_card_data,
+                india_top_card_data: india_top_card_date,
+                india_top_card_data_new:india_top_card_data_new,
+                piedata: piedata2,
+                perMillionCases: perMillionCases,
+                perMillionCasesCountry:perMillionCasesCountry,
+                bar_confirm: top_gconfirmed,
+                bar_recover: top_grecover,
+                bar_death: top_gdeaths,
+                bar_name: usedtop_gcountry,
+                timeline: timeline
+              })
+            })
+        })
     })
     .catch(function(error) {
       console.log(error);
     })
   // ----------------------------------------------------
-  axios.get(URL2)
-    .then(function(response) {
-      global_top_card_data = response.data[0];
-      india_top_card_data_new=response.data[13];
-      for (var i = 0; i < 11; i++) {
-        perMillionCases.push(response.data[i]);
-        perMillionCasesCountry.push(response.data[i].country);
-      }
 
-    //  console.log(perMillionCases); //do not delete this
-      console.log(perMillionCasesCountry[3]);
-    })
-    .catch(function(error) {
-      console.log(error);
-    })
-
-
-  axios.get(URL)
-    .then(function(response) {
-      var india_top_card_date = response.data.Countries;
-      india_top_card_date.forEach(function(item) {
-        if (item.Country === "India") {
-          specific_india_top_card_data = item;
-        }
-      })
-      top_gcountry = india_top_card_date;
-      top_gcountry.sort(function(a, b) {
-        return b.TotalConfirmed - a.TotalConfirmed
-      });
-      for (var i = 0; i < 5; i++) {
-        piedata2.push(top_gcountry[i]);
-      }
-      for (var i = 0; i < 10; i++) {
-        usedtop_gcountry.push(top_gcountry[i]);
-        top_gconfirmed.push(top_gcountry[i].TotalConfirmed);
-        top_grecover.push(top_gcountry[i].TotalRecovered);
-        top_gdeaths.push(top_gcountry[i].TotalDeaths);
-      }
-      res.render("index", {
-        global_top_card_data: global_top_card_data,
-        forindia: specific_india_top_card_data,
-        india_top_card_data: india_top_card_date,
-        india_top_card_data_new:india_top_card_data_new,
-        piedata: piedata2,
-        perMillionCases: perMillionCases,
-        perMillionCasesCountry:perMillionCasesCountry,
-        bar_confirm: top_gconfirmed,
-        bar_recover: top_grecover,
-        bar_death: top_gdeaths,
-        bar_name: usedtop_gcountry,
-        timeline: timeline
-      })
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
 });
 
 //*********************************
